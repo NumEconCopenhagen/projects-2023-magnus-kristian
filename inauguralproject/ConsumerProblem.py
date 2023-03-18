4e6f83acb6a332fe256bdd46606a7b733a7a0cbc
Kristian Ebdrup <zbp778@alumni.ku.dk>
2023-03-18 12:51:03 +0100

    Har lavet ændringer, så vi snart er færdige med op gave 1.

--- inauguralproject/ConsumerProblem.py
+++ inauguralproject/ConsumerProblem.py
@@ -53,7 +53,10 @@ class HouseholdSpecializationModelClass:
         C = par.wM*LM + par.wF*LF
 
         # b. home production
-        H = HM**(1-par.alpha)*HF**par.alpha
+        if par.sigma== 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+        else:
+            H=((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1))
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
